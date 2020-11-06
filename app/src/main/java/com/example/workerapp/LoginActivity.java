4773a310c62650826f7922f1e4dfe688e2fd868f
huizhen17 <63848933+huizhen17@users.noreply.github.com>
2020-11-06 16:06:49 +0800

    Initial commit

---
+++ app/src/main/java/com/example/workerapp/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.example.workerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void btnSignIn_onClick(View view) {
+        Intent i = new Intent(LoginActivity.this,MainActivity.class);
+        startActivity(i);
+    }
+}
