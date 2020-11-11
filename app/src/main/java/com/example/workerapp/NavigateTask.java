a60db83b4088c716532a5c95bf6e01018f3ed96d
huizhen17 <63848933+huizhen17@users.noreply.github.com>
2020-11-11 21:00:44 +0800

    Left history

--- app/src/main/java/com/example/workerapp/NavigateTask.java
+++ app/src/main/java/com/example/workerapp/NavigateTask.java
@@ -2,6 +2,10 @@ package com.example.workerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -12,14 +16,23 @@ import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.w3c.dom.Text;
 
 public class NavigateTask extends AppCompatActivity implements OnMapReadyCallback {
 
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    String userID = mAuth.getCurrentUser().getUid();
+    String orderStatus;
+    String time1, time2, serviceTime;
     MapView mvMapView;
+    TextView mtvOrderNo, mtvOrderTime, mtvOrderName, mtvOrderPhone, mbtnShareLink, metShareableLink, mbtnSendLink, mtvReSubmit;
+    Dialog sendLinkDialog;
+
     private GoogleMap gmap;
-    TextView mtvOrderNo, mtvOrderTime, mtvOrderName, mtvOrderPhone;
     private static final String MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey";
 
     @Override
@@ -32,7 +45,13 @@ public class NavigateTask extends AppCompatActivity implements OnMapReadyCallbac
         mtvOrderTime = findViewById(R.id.tvMyOrderTime);
         mtvOrderName = findViewById(R.id.tvOrderName);
         mtvOrderPhone = findViewById(R.id.tvOrderPhone);
+        mtvReSubmit = findViewById(R.id.tvReSubmit);
+        mbtnShareLink = findViewById(R.id.btnShareLink);
+        sendLinkDialog = new Dialog(this);
         mvMapView.setClickable(true);
+
+        //TODO::Get customer name, phone, address, service, amount
+        //TODO::Get booking id, date, time
 
         Bundle mapViewBundle = null;
         if (savedInstanceState != null) {
@@ -54,6 +73,7 @@ public class NavigateTask extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         gmap = googleMap;
         gmap.setMinZoomPreference(20);
+        //TODO::Retreive latitude & longitude from firebase
         LatLng ny = new LatLng(5.3338433, 100.2771833);
         gmap.addMarker(new MarkerOptions().position(ny).title("Customer's House"));
         gmap.moveCamera(CameraUpdateFactory.newLatLngZoom(ny,20F));
@@ -65,7 +85,53 @@ public class NavigateTask extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     public void btnShareLink_onClick(View view) {
-        //TODO::Display dialog box to shareable link
+        //TODO::Calculate Start Time
+        displayDialog();
+
+        mtvReSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayDialog();
+            }
+        });
+
+        mbtnShareLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO::Calculate End Time
+                //TODO::Calculate difference between two times
+                //TODO::Intent time,name,ammount,service -> intent whole orderdetail class
+                orderStatus = "arrived";
+                Intent i = new Intent(NavigateTask.this,ReceivePayment.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    private void displayDialog() {
+        sendLinkDialog.setContentView(R.layout.submit_link_dialog);
+        sendLinkDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        sendLinkDialog.show();
+        sendLinkDialog.setCancelable(false);
+
+        metShareableLink = sendLinkDialog.findViewById(R.id.etShareableLink);
+        mbtnSendLink = sendLinkDialog.findViewById(R.id.btnSendLink);
+
+        mbtnSendLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(metShareableLink.getText().toString().isEmpty()){
+                    metShareableLink.setError("Please paste the link here");
+                    metShareableLink.requestFocus();
+                }else{
+                    String link = metShareableLink.getText().toString();
+                    //TODO::Store link to db
+                    mbtnShareLink.setText("I'm Arrived");
+                    sendLinkDialog.dismiss();
+                    mtvReSubmit.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 
     public void ivPhone_onClick(View view) {
--- app/src/main/java/com/example/workerapp/ReceivePayment.java
+++ app/src/main/java/com/example/workerapp/ReceivePayment.java
@@ -2,13 +2,35 @@ package com.example.workerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ReceivePayment extends AppCompatActivity {
+
+    TextView mtvOrderName, mtvOrderAmount, mtvOrderService, mtvMyBalance;
+    Button mbtnReceived;
+    ImageView mivQrCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_payment);
+
+        mtvOrderName = findViewById(R.id.tvOrderName);
+        mtvOrderAmount = findViewById(R.id.tvOrderAmount);
+        mtvOrderService = findViewById(R.id.tvOrderService);
+        mtvMyBalance = findViewById(R.id.tvMyBalance);
+        mbtnReceived = findViewById(R.id.btnReceived);
+        mivQrCode = findViewById(R.id.ivQrCode);
+
+    }
+
+    public void btnReceived_onClick(View view) {
+        Intent i = new Intent(ReceivePayment.this,CompletePayment.class);
+        startActivity(i);
     }
 }
